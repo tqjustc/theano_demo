84e09c200d545c604b050e9fbb7119b2422480d9
Qiongjie TIan <tqjustc@gmail.com>
2017-05-13 21:24:07 -0700

    upload the entire file.

--- sharedVariable.py
+++ sharedVariable.py
@@ -5,3 +5,9 @@ import theano.tensor as T
 state = theano.shared(0)
 inc = T.iscalar('inc')
 accumulator = theano.function([inc], state, updates = [(state, state+inc)])
+
+print(state.get_value())
+accumulator(2)
+print (state.get_value())
+accumulator(3)
+print (state.get_value())
