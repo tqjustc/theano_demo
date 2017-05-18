12ac3d525c5bbef863cf0b42ac536ad2ca34a57a
Qiongjie TIan <tqjustc@gmail.com>
2017-05-17 22:46:13 -0700

    waiting to be complete on home_ubuntu

---
+++ LogisticRegression_binary.py
@@ -0,0 +1,10 @@
+import pdb
+import theano
+import theano.tensor as T
+import numpy as np
+
+class LogisticRegression_binary(object):
+    def __init__(self, input, ):
+        self.w = w
+        self.b = b
+
