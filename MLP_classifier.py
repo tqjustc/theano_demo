20d14aa5f7a99456701f9efe504bafbc71c8d93d
Qiongjie TIan <tqjustc@gmail.com>
2017-05-21 22:10:24 -0700

    Demo for MLP, waiting to be complete for the Hidden Layer.

--- MLP_classifier.py
+++ MLP_classifier.py
@@ -4,6 +4,7 @@ import theano
 import theano.tensor as T
 import gzip
 import numpy as np
+import os
 
 class LogisticRegression(object):
     def __init__(self, input, n_in, n_out):
@@ -35,3 +36,32 @@ class LogisticRegression(object):
             raise ValueError('Wrong input y.')
         else:
             return T.mean(T.neq(y, self.y_pred))
+
+def load_data():
+    url1 = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+    from six.moves import urllib
+    import six.moves.cPickle as Pickle
+
+    dataset = os.getcwd() + '/data/'
+    urllib.request.urlretrieve(url1, dataset)
+    with gzip.open(dataset, 'rb') as f:
+        train_set, valid_set, test_set = Pickle.load(f)
+
+    def shared_dataset(data_xy, borrow=True):
+        x, y = data_xy
+        xx = theano.shared(
+            value=np.asarray(x, dtype=theano.config.floatX),
+            name='xx',
+            borrow=borrow
+        )
+        yy = theano.shared(
+            value=np.asarray(y, dtype=theano.config.floatX),
+            name='yy',
+            borrow=borrow
+        )
+        return xx, yy
+    train_set_x, train_set_y = shared_dataset(train_set)
+    valid_set_x, valid_set_y = shared_dataset(valid_set)
+    test_set_x, test_set_y = shared_dataset(test_set)
+
+    return [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
